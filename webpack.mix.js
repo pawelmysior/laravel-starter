db9b2c46da86c9ce774759e4395067ed60ab2826
Pawel Mysior <pawelmysior@gmail.com>
2019-04-15 15:05:33 +0200

    Add laravel-mix-purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,9 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 mix
   .js('resources/js/app.js', 'public/js')
   .postCss('resources/css/app.css', 'public/css', [
     require('tailwindcss')('./resources/css/tailwind.config.js'),
-  ]);
-
+  ])
+  .purgeCss();
