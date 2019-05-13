ee77ce71c99634ce731e5400c3218f3471c7ed2f
Pawel Mysior <pawelmysior@gmail.com>
2019-05-13 10:29:32 +0200

    Use the vue-renderless-components package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import Vue from 'vue';
+import VueRenderlessComponents from 'vue-renderless-components';
+
+Vue.use(VueRenderlessComponents);
 
 axios.defaults.headers.common['X-CSRF-TOKEN'] = document.head.querySelector('meta[name="csrf-token"]').content;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
