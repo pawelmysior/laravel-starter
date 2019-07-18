bb289fe1d37015995a1c22b6c7108be6fcad2a4f
Pawel Mysior <pawelmysior@gmail.com>
2019-07-18 16:23:35 +0200

    Extend fonts rather than replace them

--- resources/css/tailwind.config.js
+++ resources/css/tailwind.config.js
@@ -1,21 +1,23 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      sans: [
-        'Open Sans',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
+    extend: {
+      fontFamily: {
+        sans: [
+          'Open Sans',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
     },
   },
   variants: {},
