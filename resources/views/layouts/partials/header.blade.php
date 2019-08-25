a0b2220a6aaecc419642589a1cc290b4b108fb2e
Pawel Mysior <pawelmysior@gmail.com>
2019-08-25 12:13:36 +0200

    Make the buttons bigger

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -5,10 +5,10 @@
         </a>
 
         <button class="flex xl:hidden items-center px-4 py-4 appearance-none focus:outline-none" @click="toggle">
-            <svg class="fill-current h-3 w-3" :class="{ hidden: hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" v-cloak>
+            <svg class="fill-current h-4 w-4" :class="{ hidden: hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" v-cloak>
                 <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
             </svg>
-            <svg class="fill-current h-3 w-3" :class="{ hidden: !hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" v-cloak>
+            <svg class="fill-current h-4 w-4" :class="{ hidden: !hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" v-cloak>
                 <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
             </svg>
         </button>
